739c76e7cbd3d4187a5365924d57da035e58d5d7
Andrea Amerigo Aristodemo Gariboldi <andrea.gariboldi@gmail.com>
2014-05-01 13:35:33 +0200

    initial

---
+++ index.js
@@ -0,0 +1,67 @@
+var clone= require('circularclone'),
+    traverse= require('circularjs'),
+    _= require('underscore');
+
+module.exports= function (opts)
+{
+   opts= _.defaults(opts,{ key: '_id', ref: '_ref' });
+
+   var idmap= {},
+       _contains= function (val)
+       {
+          return val&&val[opts.key]&&!!idmap[val[opts.key]];
+       },
+       _get= function (key)
+       {
+          return idmap[key];
+       },
+       _set= function (key,node)
+       {
+          idmap[key]= node;
+       };
+
+   if (opts.graph)
+     traverse(opts.graph,
+     function (node)
+     {
+        var key= node[opts.key];
+
+        if (key!==undefined)
+          _set(key,node);
+     });
+
+   return {
+             set: _set,
+             unset: function (key)
+             {
+                idmap[key]= undefined;
+             },
+             get: _get,
+             contains: _contains,
+             detach: function (sub)
+             {
+                return clone(sub,function (key,clonedValue,clone,node,origValue)
+                {
+                  if (_contains(origValue))
+                  {
+                     var r= {};
+                     r[opts.ref]= origValue[opts.key];
+                     return r;
+                  }
+                  else
+                    return clonedValue;
+                },
+                _contains);
+             },
+             attach: function (sub)
+             {
+                return clone(sub,function (key,clonedValue,clone,node,origValue)
+                {
+                  if (origValue[opts.ref])
+                    return _get(origValue[opts.ref]);
+                  else
+                    return clonedValue;
+                });
+             } 
+          };
+};
---
+++ test/detach.test.js
@@ -0,0 +1,42 @@
+var should= require('chai').should(),
+    assert= require('chai').assert,
+    region=  require('../index.js');
+
+var cir= function ()
+{
+    var a= { nome: 'Andrea' },
+        e= { nome: 'Elena' };
+
+    a.figlia= e;
+    a.figlia2= e;
+    e.papa= a;
+
+    return [a,3,[e,a]];
+};
+
+describe('region',function ()
+{
+       it('can detach a subgraph from a graph, and reattach it', function (done)
+       {
+           var gph= cir(),
+               reg= region({ graph: gph, key: 'nome' }),
+               sub= { a: gph[0], e: gph[2][0], x: 5, arr: [gph[0],gph[2][0]] },
+               detached= reg.detach(sub),
+               attached= reg.attach(detached);
+
+           should.exist(detached); 
+           should.exist(detached.a._ref);
+           detached.a._ref.should.equal('Andrea');
+           detached.e._ref.should.equal('Elena');
+           detached.arr[0]._ref.should.equal('Andrea');
+           detached.arr[1]._ref.should.equal('Elena');
+           detached.x.should.equal(5);
+
+           attached.a.should.equal(gph[0]); 
+           attached.e.should.equal(gph[2][0]); 
+           attached.arr[0].should.equal(gph[0]); 
+           attached.arr[1].should.equal(gph[2][0]); 
+           attached.x.should.equal(5); 
+           done();
+       });
+});
